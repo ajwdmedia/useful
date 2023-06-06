673d2cf2592287b58d498c5156e2a14e1a93a996
ajwdmedia <ajwdmedia@gmail.com>
2023-06-06 22:19:51 +0000

    adjust valueWritables they now allow not having an initial value, but do not allow getting or updating before setting.

--- src/lib/stores/value.ts
+++ src/lib/stores/value.ts
@@ -1,9 +1,11 @@
 import { writable, type Writable } from "svelte/store";
 
+type NonUndefined<T> = T extends undefined ? never : T;
+
 export type ValueWritable<T> = Writable<T> & { value: () => T };
 
-export function valueWritable<T> (init: T): ValueWritable<T> {
-    let value = init;
+export function valueWritable<T> (init?: T): ValueWritable<T> {
+    let value: T = init ?? undefined as T;
     let wr = writable(init);
     return {
         subscribe: wr.subscribe,
@@ -11,8 +13,9 @@ export function valueWritable<T> (init: T): ValueWritable<T> {
             wr.set(value = newValue)
         },
         update(fn: (oldValue: T) => T) {
+            if (value === undefined) throw new Error("Value not set before update")
             wr.set(value = fn(value));
         },
-        value: () => value
+        value: () => { if (value === undefined) { throw new Error("Value not set") } return value; }
     };
 }
